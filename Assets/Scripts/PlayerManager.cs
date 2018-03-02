dc5b0014c353c5433fe13e2644c6c00c3cfdc79a
nick76567 <kcwongay@connect.ust.hk>
2018-03-02 15:23:54 +0800

    player change to destroy onLoad, 4 players seems ok now

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class PlayerManager : Photon.PunBehaviour, IPunObservable {
+public class PlayerManager : Photon.PunBehaviour, IPunObservable{
+
+    private const int READY_BUTTON = 6;
+    private const string SCENE = "Game";
 
     public GameObject[] allButtonsList;
     public GameObject[] allIdleCharactersList;
+    public Text readyButton;
 
     private string[] charactersName = { "Golem", "Grunt", "Lich", "Soldier" };
     private enum CharactersName { Golem, Grunt, Lich, Soldier, CharatersLen};
     private CharactersName currentCharacter;
+    private bool isReady , isBeingMasterClient;
+    private static int totalIsReady, maxPlayerPerTeam;
+
     // Use this for initialization
     void Start () {
         if (photonView.isMine)
@@ -18,6 +26,20 @@ public class PlayerManager : Photon.PunBehaviour, IPunObservable {
 
             for (int i = 1; i < (int)CharactersName.CharatersLen; i++)
                 allIdleCharactersList[i].SetActive(false);
+
+            isBeingMasterClient = false;
+            isReady = false;
+            totalIsReady = 0;
+            
+            if (PhotonNetwork.isMasterClient)
+            {
+                isBeingMasterClient = true;
+                allButtonsList[READY_BUTTON].GetComponent<Button>().enabled = false;
+                readyButton.text = "Start";
+            }
+
+            SceneManager.sceneLoaded += this.OnLoadCallBack;
+
         }
         else
         {
@@ -30,17 +52,55 @@ public class PlayerManager : Photon.PunBehaviour, IPunObservable {
             {
                 idleCharacter.SetActive(false);
             }
+
         }
+        maxPlayerPerTeam = PhotonNetwork.room.MaxPlayers / 2;
 
-        DontDestroyOnLoad(this);
+        //DontDestroyOnLoad(this);
 
-        Debug.Log("Player is created");
+
+        Debug.Log("photon maxplayer " + PhotonNetwork.room.MaxPlayers);
+        Debug.Log("MaxPlayerPerTeam " + maxPlayerPerTeam);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (PhotonNetwork.isMasterClient && photonView.isMine)
+        {
+            if (!isBeingMasterClient)
+            {
+                if (isReady)
+                {
+                    this.photonView.RPC("RPCReady", PhotonTargets.All, null);
+                }
+                allButtonsList[READY_BUTTON].GetComponent<Button>().enabled = false;
+                readyButton.text = "Start";
+                isBeingMasterClient = true;
+            }
+/*
+            Debug.Log("Red Team " + PunTeams.PlayersPerTeam[PunTeams.Team.red].Count);
+            Debug.Log("Blue Team " + PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count);
+            Debug.Log("totalIsReady " + totalIsReady);
+*/
+            if (PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count == maxPlayerPerTeam &&
+                PunTeams.PlayersPerTeam[PunTeams.Team.red].Count == maxPlayerPerTeam &&
+                totalIsReady == PhotonNetwork.room.MaxPlayers - 1)
+            {
+                allButtonsList[READY_BUTTON].GetComponent<Button>().enabled = true;
+            }
+            
+        }
+
+
+    }
+
+    private void OnLoadCallBack(Scene scene, LoadSceneMode sceneMode)
+    {
+        if(scene.name == "Game")
+        {
+            PhotonNetwork.Instantiate(charactersName[(int)currentCharacter], new Vector3(Random.Range(0, 100), 0, 0), Quaternion.identity, 0);
+        }
+    }
     
     private void SelectCharacter(CharactersName name)
     {
@@ -69,21 +129,51 @@ public class PlayerManager : Photon.PunBehaviour, IPunObservable {
         SelectCharacter(CharactersName.Soldier);
     }
 
-    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    public void ChangeRedTeam()
     {
-        if (stream.isWriting)
-        {
+        PhotonNetwork.player.SetTeam(PunTeams.Team.red);
+        
+    }
 
+    public void ChangeBlueTeam()
+    {
+        PhotonNetwork.player.SetTeam(PunTeams.Team.blue);
+        
+    }
+
+    public void Ready()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            PhotonNetwork.LoadLevel(SCENE);
         }
         else
         {
-
+            this.photonView.RPC("RPCReady", PhotonTargets.All, null);
         }
+        
     }
 
     [PunRPC]
-    public void Ready()
+    public void RPCReady()
     {
+            if (isReady)
+            {
+                isReady = false;
+                totalIsReady--;
+                Debug.Log("RPCREady in isReady " + totalIsReady);
+            }
+            else
+            {
 
+                isReady = true;
+                totalIsReady++;
+                Debug.Log("RPCREady in !isReady " + totalIsReady);
+            }
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        
     }
 }
