dbc4023fbe7fd456b55cd63fa6629bb9d4bade6a
kayiuc <danielkayiu@hotmail.com>
2018-04-18 15:31:19 +0800

    bgm done

---
+++ PC/Assets/Music 1/DontDestroyMusic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyMusic : MonoBehaviour {
+
+	static DontDestroyMusic instance = null;
+	void Awake()
+	{
+		if (instance != null)
+		{
+			if (instance != this)
+				Destroy(instance.gameObject);
+			else
+				Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
